919f9ea7375cd3d6e6157f4c71e9634e5edbc2fa
Ghislain Antony Vaillant <ghisvail@gmail.com>
2015-07-20 18:20:14 +0100

    add impl. of GiplInput::read_native_scanline

--- src/gipl.imageio/giplinput.cpp
+++ src/gipl.imageio/giplinput.cpp
@@ -67,8 +67,10 @@ GiplInput::open (const std::string &name, ImageSpec &spec)
     return false;
   }
 
-  m_spec = ImageSpec(m_header.dim[0], m_header.dim[1], 1);
-
+  m_spec = ImageSpec();
+  m_spec.width = m_header.dim[0];
+  m_spec.height = m_header.dim[1];
+  m_spec.depth = m_header.dim[2];
   m_spec.attribute("XResolution", m_header.pixdim[0]);
   m_spec.attribute("YResolution", m_header.pixdim[1]);
   m_spec.attribute("ResolutionUnit", "mm");
@@ -99,6 +101,7 @@ GiplInput::open (const std::string &name, ImageSpec &spec)
     case gipl_pvt::IT_DOUBLE:
       m_spec.set_format(TypeDesc::DOUBLE);
       break;
+/*
     case gipl_pvt::IT_VECTOR_3D_CHAR:
       m_spec.set_format(TypeDesc(TypeDesc::INT8, TypeDesc::VEC3));
       break;
@@ -123,6 +126,7 @@ GiplInput::open (const std::string &name, ImageSpec &spec)
     case gipl_pvt::IT_C_DOUBLE:
       m_spec.set_format(TypeDesc(TypeDesc::DOUBLE, TypeDesc::VEC2));
       break;
+*/
     default:
       error("Image type not supported");
       close();
@@ -169,6 +173,63 @@ GiplInput::open (const std::string &name, ImageSpec &spec)
   return true;
 }
 
+bool
+GiplInput::read_native_scanline(int y, int z, void *data)
+{
+  std::vector<unsigned char> scanline_data(m_spec.scanline_bytes());
+  long scanline_offset = (m_spec.height - y) * m_spec.scanline_bytes();
+
+  // position file pointer to beginning of scanline
+  fseek(m_fd, scanline_offset, SEEK_CUR);
+
+  // read content of scanline
+  fread(&scanline_data[0], sizeof(unsigned char), scanline_data.size());
+  
+  // GIPL format is big-endian, swap if required
+  if (littleendian ())
+  {
+    if(m_spec.format == TypeDesc::UINT8)
+      swap_endian((uint8_t*)&scanline_data[0],
+        scanline_data.size()/sizeof(uint8_t));
+
+    else if(m_spec.format == TypeDesc::INT8)
+      swap_endian((int8_t*)&scanline_data[0],
+        scanline_data.size()/sizeof(int8_t));
+
+    else if(m_spec.format == TypeDesc::UINT16)
+      swap_endian((uint16_t*)&scanline_data[0],
+        scanline_data.size()/sizeof(uint16_t));
+
+    else if(m_spec.format == TypeDesc::INT16)
+      swap_endian((int16_t*)&scanline_data[0],
+        scanline_data.size()/sizeof(int16_t));
+
+    else if(m_spec.format == TypeDesc::UINT)
+      swap_endian((uint32_t*)&scanline_data[0],
+        scanline_data.size()/sizeof(uint32_t));
+
+    else if(m_spec.format == TypeDesc::INT)
+      swap_endian((int32_t*)&scanline_data[0],
+        scanline_data.size()/sizeof(uint32_t));
+
+    else if(m_spec.format == TypeDesc::FLOAT)
+      swap_endian((float*)&scanline_data[0],
+        scanline_data.size()/sizeof(float));
+
+    else if(m_spec.format == TypeDesc::DOUBLE)
+      swap_endian((double*)&scanline_data[0],
+        scanline_data.size()/sizeof(double));
+  }
+
+  // copy scanline content to output buffer
+  memcpy(data, &scanline_data[0], scanline_data.size());
+  
+  // reset file pointer to beginning of data
+  fseek(m_fd, gipl_pvt::GIPL_HEADER_SIZE, SEEK_SET);
+  
+  return true;
+}
+
 void
 GiplInput::init () {
   m_fd = NULL;
@@ -177,30 +238,30 @@ GiplInput::init () {
 
 bool
 GiplInput::read_header() {
-  if( !fread(&m_header.dim, sizeof(m_header.dim), 1) ||
-      !fread(&m_header.image_type, sizeof(m_header.image_type), 1) ||
-      !fread(&m_header.pixdim, sizeof(m_header.pixdim), 1) ||
-      !fread(&m_header.line1, sizeof(m_header.line1), 1) ||
-      !fread(&m_header.matrix, sizeof(m_header.matrix), 1) ||
-      !fread(&m_header.flag1, sizeof(m_header.flag1), 1) ||
-      !fread(&m_header.flag2, sizeof(m_header.flag2), 1) ||
-      !fread(&m_header.min, sizeof(m_header.min), 1) ||
-      !fread(&m_header.max, sizeof(m_header.max), 1) ||
-      !fread(&m_header.origin, sizeof(m_header.origin), 1) ||
-      !fread(&m_header.pixval_offset, sizeof(m_header.pixval_offset), 1) ||
-      !fread(&m_header.pixval_cal, sizeof(m_header.pixval_cal), 1) ||
-      !fread(&m_header.user_def1, sizeof(m_header.user_def1), 1) ||
-      !fread(&m_header.user_def2, sizeof(m_header.user_def2), 1) ||
-      !fread(&m_header.magic_number, sizeof(m_header.magic_number), 1) )
+  if( !fread(&m_header.dim)           ||
+      !fread(&m_header.image_type)    ||
+      !fread(&m_header.pixdim)        ||
+      !fread(&m_header.line1)         ||
+      !fread(&m_header.matrix)        ||
+      !fread(&m_header.flag1)         ||
+      !fread(&m_header.flag2)         ||
+      !fread(&m_header.min)           ||
+      !fread(&m_header.max)           ||
+      !fread(&m_header.origin)        ||
+      !fread(&m_header.pixval_offset) ||
+      !fread(&m_header.pixval_cal)    ||
+      !fread(&m_header.user_def1)     ||
+      !fread(&m_header.user_def2)     ||
+      !fread(&m_header.magic_number)  )
   {
-    error ("Could not read header of file \"%s\"", name.c_str());
+    error ("Could not read header of file \"%s\"", m_filename.c_str());
     return false;
   }
 
   // ensure line1 attribute is a well-formed c-string 
   m_header.line1[79] = '\0';
 
-  // GIPL format is written as big-endian
+  // GIPL format is big-endian, swap if required
   if(littleendian())
   {
     swap_endian(&m_header.dim, 4);
